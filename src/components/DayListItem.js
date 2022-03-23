5a20e6ce59fbf7e81bc70acf0a8109a38dfd9145
davincecode <hyperscript.ca@gmail.com>
2022-03-22 21:07:49 -0600

    Update test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,24 +2,27 @@ import React from "react"
 import "components/DayListItem.scss"
 import classNames from "classnames"
 
-// export default function DayListItem(props) {
-//   return (
-//     <li onClick={() => props.setDay(props.name)}>
-//       <h2 className="text--regular">{props.name}</h2>
-//       <h3 className="text--light">{props.spots} spots remaining</h3>
-//     </li>
-//   )
-// }
 export default function DayListItem(props) {
-  const dayClass = classNames("li", "day-list__item", {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
   })
 
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return <h3 className="dayClass">no spots remaining</h3>
+    } else if (props.spots === 1) {
+      return <h3 className="dayClass">1 spot remaining</h3>
+    } else if (props.spots > 1) {
+      return <h3 className="dayClass">{props.spots} spots remaining</h3>
+    }
+  }
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2>{props.name}</h2>
-      <h3>{props.spots} spots remaining</h3>
+      <h2 className="dayClass">{props.name}</h2>
+      {formatSpots()}
     </li>
   )
 }
