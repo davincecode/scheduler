5bdd5409b451315f80183354ceeccef4ec755cab
davincecode <hyperscript.ca@gmail.com>
2022-03-22 20:14:23 -0600

    Update DayListItem

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,22 @@ import React from "react"
 import "components/Button.scss"
 import classNames from "classnames"
 
+export default function Button(props) {
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  })
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  )
+}
 // export default function Button(props) {
 //   let buttonClass = "button"
 
@@ -22,20 +38,3 @@ import classNames from "classnames"
 //     </button>
 //   )
 // }
-
-export default function Button(props) {
-  const buttonClass = classNames("button", {
-    "button--confirm": props.confirm,
-    "button--danger": props.danger,
-  })
-
-  return (
-    <button
-      className={buttonClass}
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      {props.children}
-    </button>
-  )
-}
