853dc7635e0408c9399c8996d1e5fb6a78037dbc
davincecode <hyperscript.ca@gmail.com>
2022-03-23 22:37:07 -0600

    Update Show component

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,8 @@ import InterviewerList from "components/InterviewerList"
 import InterviewerListItem from "components/InterviewerListItem"
 import Appointment from "components/Appointment/index.js"
 import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 storiesOf("Button", module)
   .addParameters({
@@ -136,3 +138,11 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show
+      interviewers={interviewers}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
