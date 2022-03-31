3b9915eb83cf5048d2f1c377fb6c0520ff3c354e
davincecode <hyperscript.ca@gmail.com>
2022-03-30 22:36:19 -0600

    Update readme

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # Interview Scheduler
 
+## Built using ReactJS
+
+##FEATURES
+
+### ADD BOOKING
+
+![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
+
+### EDIT BOOKING
+
+![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
+
+### DELETE BOOKING
+
+![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
+
+### CANCEL
+
+![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
+
 ## Setup
 
 Install dependencies with `npm install`.
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,41 +1,76 @@
 export function getAppointmentsForDay(state, day) {
-  const { days, appointments } = state
-  const dayFromState = days.filter((el) => el.name === day)
+  const filteredDays = state.days.filter((stateDay) => stateDay.name === day)
 
-  let ids
-  dayFromState[0] ? (ids = dayFromState[0].appointments) : (ids = [])
+  if (filteredDays.length === 0) return []
 
-  if (appointments) {
-    return Object.values(appointments).filter((el) => ids.includes(el.id))
-  } else {
-    return []
-  }
+  const appointmentsArr = filteredDays[0].appointments.map((id) => {
+    return state.appointments[id]
+  })
+  return appointmentsArr
 }
 
+/* Gets interviewers for specific day and returns array of interviewers */
 export function getInterviewersForDay(state, day) {
-  const { days, interviewers } = state
-  const dayFromState = days.filter((el) => el.name === day)
+  const filteredDays = state.days.filter((stateDay) => stateDay.name === day)
 
-  let ids
-  dayFromState[0] ? (ids = dayFromState[0].interviewers) : (ids = [])
+  if (filteredDays.length === 0) return []
 
-  if (interviewers) {
-    return Object.values(interviewers).filter((el) => ids.includes(el.id))
-  } else {
-    return []
-  }
+  const interviewersArr = filteredDays[0].interviewers.map((id) => {
+    return state.interviewers[id]
+  })
+  return interviewersArr
 }
 
+/* Returns object with student and interview object */
 export function getInterview(state, interview) {
-  let id = interview ? interview.interviewer : null
-
-  if (id) {
-    const obj = {
-      student: interview.student,
-      interviewer: Object.values(state.interviewers)[id - 1],
-    }
-    return obj
-  } else {
+  if (!interview) {
     return null
   }
+
+  return {
+    ...interview,
+    interviewer: state.interviewers[interview.interviewer],
+  }
 }
+
+// export function getAppointmentsForDay(state, day) {
+//   const { days, appointments } = state
+//   const dayFromState = days.filter((el) => el.name === day)
+
+//   let ids
+//   dayFromState[0] ? (ids = dayFromState[0].appointments) : (ids = [])
+
+//   if (appointments) {
+//     return Object.values(appointments).filter((el) => ids.includes(el.id))
+//   } else {
+//     return []
+//   }
+// }
+
+// export function getInterviewersForDay(state, day) {
+//   const { days, interviewers } = state
+//   const dayFromState = days.filter((el) => el.name === day)
+
+//   let ids
+//   dayFromState[0] ? (ids = dayFromState[0].interviewers) : (ids = [])
+
+//   if (interviewers) {
+//     return Object.values(interviewers).filter((el) => ids.includes(el.id))
+//   } else {
+//     return []
+//   }
+// }
+
+// export function getInterview(state, interview) {
+//   let id = interview ? interview.interviewer : null
+
+//   if (id) {
+//     const obj = {
+//       student: interview.student,
+//       interviewer: Object.values(state.interviewers)[id - 1],
+//     }
+//     return obj
+//   } else {
+//     return null
+//   }
+// }
