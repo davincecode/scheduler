231623750f268afd7e76aed64988f1c1aafcda84
davincecode <hyperscript.ca@gmail.com>
2022-03-30 22:07:27 -0600

    Removed comments

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -12,7 +12,6 @@ export default function reducer(state, action) {
       const { days, appointments, interviewers } = action
       return { ...state, days, appointments, interviewers }
     case SET_INTERVIEW: {
-      // console.log("ACTION: ", action);
       const { id, interview } = action
       const appointment = {
         ...state.appointments[id],
@@ -36,12 +35,9 @@ export default function reducer(state, action) {
 }
 
 function updateSpots(state, appointments, id) {
-  // make copy of days array
   const days = state.days.map((day) => {
     return { ...day }
   })
-
-  // console.log("DAYS: ", days);
 
   function findDay(day) {
     const daysOfWeek = {
@@ -53,27 +49,20 @@ function updateSpots(state, appointments, id) {
     }
     return daysOfWeek[day]
   }
-  // find the day's index in days array
+
   const dayIndex = findDay(state.day)
-  // console.log("STATE.DAY: ", state.day)
-  // console.log("DAYINDEX: ", dayIndex)
 
-  // check for old state
   const prevState = state.appointments[id].interview
-  // console.log("prevSTATE: ", prevState);
-  // check for new state
-  const newState = appointments[id].interview
-  // console.log("newSTATE: ", newState);
 
-  // create - no old state + new state
+  const newState = appointments[id].interview
+
   if (!prevState && newState) {
     days[dayIndex].spots--
   }
-  // delete - old state + no new state
+
   if (prevState && !newState) {
     days[dayIndex].spots++
   }
 
-  // return days array
   return days
 }
